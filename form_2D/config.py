9f38022431e739189c596f28c713f8c206e83773
nguacon01 <dungdm6191@gmail.com>
2020-11-13 13:42:30 +0100

    add login required for index site

--- form_2D/config.py
+++ form_2D/config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = "X8slQiQWkvC0Zytlrntx9NQB009oOOg5r5kiah68NkckksDyuguwkz0KCV9lK3P5"
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "site.db")
-    SQL_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_TOKEN_LOCATION = "cookies"
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
     
