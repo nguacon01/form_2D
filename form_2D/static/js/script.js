d5cb3c9b63d583ef715039d7f7aeda50a0d16751
nguacon01 <dungdm6191@gmail.com>
2021-03-08 19:23:20 +0100

    add css btn-danger. Change css login page

--- form_2D/static/js/script.js
+++ form_2D/static/js/script.js
@@ -16,22 +16,7 @@ $(document).ready(function(){
         e.preventDefault();
         $('body').removeClass('nav-expanded');
     });
-    // Initialize navgoco with default options
-    $(".main-menu").navgoco({
-        caret: '<span class="caret"></span>',
-        accordion: false,
-        openClass: 'open',
-        save: true,
-        cookie: {
-            name: 'navgoco',
-            expires: false,
-            path: '/'
-        },
-        slide: {
-            duration: 300,
-            easing: 'swing'
-        }
-    });
+    
     // -----------------------------------------------------------------
     // SMOOTH SCROLL
     // -----------------------------------------------------------------
